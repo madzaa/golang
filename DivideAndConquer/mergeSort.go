a5f1a8ec0c2096684f18256b6695070dac404068
Madis Salumäe <madis.salumae@luminorgroup.com>
2019-08-30 21:31:53 +0300

    make it easier for myself to understand what is going on

--- DivideAndConquer/mergeSort.go
+++ DivideAndConquer/mergeSort.go
@@ -5,22 +5,22 @@ import "fmt"
 func merge(l, r []int) []int {
 	finalArray := make([]int, 0, len(l)+len(r))
 	for len(l) > 0 || len(r) > 0 {
-		fmt.Println("Array is", l, r)
+		fmt.Println("Arrays are", l, r)
 		if len(l) == 0 {
-			fmt.Println("len is ", len(l), "append", r)
+			fmt.Println("Nothing left in the left array ", len(l), "so we append", r[0])
 			return append(finalArray, r...)
 		}
 		if len(r) == 0 {
-			fmt.Println("len is ", len(r), "append", l)
+			fmt.Println("Nothing left in right array ", len(r), "so we append", l[0])
 			return append(finalArray, l...)
 		}
 		if l[0] <= r[0] {
 			finalArray = append(finalArray, l[0])
-			fmt.Println(r[0], "is bigger than", l[0], "append", l[0])
+			fmt.Println(r[0], "is bigger than", l[0], "so we append", l[0])
 			l = l[1:]
 		} else {
 			finalArray = append(finalArray, r[0])
-			fmt.Println(l[0], "is bigger than", r[0], "append", r[0])
+			fmt.Println(l[0], "is bigger than", r[0], "so we append", r[0])
 			r = r[1:]
 		}
 	}
